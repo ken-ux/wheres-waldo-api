c969c97307f216d49f3c34a3076d7f474708fe63
Kenny Nguyen <sokenns@gmail.com>
2024-06-18 14:54:40 -0700

    Add insert statements

--- create-tables.sql
+++ create-tables.sql
@@ -18,10 +18,78 @@ CREATE TABLE
 
 -- Create user table for leaderboard scores.
 CREATE TABLE
-    "user" ( -- This is in quotes because user is a reserved word.
+    "user" ( -- This is in double quotes because user is a reserved word.
         user_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
         game_id SMALLINT NOT NULL,
         user_name VARCHAR(20) NOT NULL,
         user_score INTEGER,
         FOREIGN KEY (game_id) REFERENCES game (game_id)
+    );
+
+-- Insert data into game table since it has no FKs.
+INSERT INTO
+    game (game_name)
+VALUES
+    ('easy'), -- Can't use double quotes otherwise throws a syntax error.
+    ('medium'),
+    ('hard');
+
+-- Insert data into goal table.
+INSERT INTO
+    goal (game_id, goal_desc, goal_pos_x, goal_pos_y)
+VALUES
+    (
+        (
+            SELECT
+                game_id
+            FROM
+                game
+            WHERE
+                game_name = 'medium'
+        ),
+        'This is the first goal.',
+        125,
+        711
+    ),
+    (
+        (
+            SELECT
+                game_id
+            FROM
+                game
+            WHERE
+                game_name = 'hard'
+        ),
+        'This is the second goal.',
+        500,
+        212
+    );
+
+-- Insert data into user table as a test.
+INSERT INTO
+    "user" (game_id, user_name, user_score)
+VALUES
+    (
+        (
+            SELECT
+                game_id
+            FROM
+                game
+            WHERE
+                game_name = 'easy'
+        ),
+        'Michael',
+        120
+    ),
+    (
+        (
+            SELECT
+                game_id
+            FROM
+                game
+            WHERE
+                game_name = 'hard'
+        ),
+        'Kelly',
+        500
     );
